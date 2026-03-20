fe2bf40af2b3f184bbe9e014cad386f43203d357
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2026-03-20 22:18:42 +0900

    chore: update vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,13 @@ import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./tests/vitest-setup.ts'],
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
   },
 });
